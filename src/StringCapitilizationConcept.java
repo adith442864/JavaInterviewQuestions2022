3728f33edd60ab75f19aea86e84ace82eb31ddc1
adith442864 <adith442864@gmail.com>
2022-05-14 19:41:20 +0530

    140522

--- src/StringCapitilizationConcept.java
+++ src/StringCapitilizationConcept.java
@@ -3,7 +3,7 @@ public class StringCapitilizationConcept {
 
 	public static void main(String[] args) {
 		
-		String str = "hello how are you";
+		String str = "hello hOw aRe yOu";
 		
 		String words[] = str.split(" ");
 		
@@ -11,7 +11,7 @@ public class StringCapitilizationConcept {
 		
 		for(String word: words) {
 			String first = word.substring(0,1);
-			String remaining = word.substring(1);
+			String remaining = word.substring(1).toLowerCase();
 			
 			capitalizeStr += first.toUpperCase()+remaining+" ";
 			
